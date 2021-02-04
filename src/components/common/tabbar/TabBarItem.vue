2e6c3d3b5f71f5d99cab71a63f55c6c412bffa2f
zjc <1738791930@qq.com>
2021-02-04 16:40:38 +0800

    'vue'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="tab-bar-item" @click="itemclick">
+        <div v-if=isActive><slot name="item-icon"></slot></div>
+        <div v-else><slot name="item-iconf"></slot></div>
+        <div :style="activestyle"><slot name="item-text"></slot></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarItem",
+        props: {
+            path: String,
+            activecolor: {
+                type: String,
+                default: 'pink'
+            }
+        },
+        data(){
+            return{
+                //isActive: true
+            }
+        },
+        computed: {
+            isActive(){
+                return this.$route.path.indexOf(this.path) == -1
+            },
+            activestyle(){
+                return this.isActive ? {}:{color: this.activecolor}
+            }
+        },
+        methods: {
+            itemclick(){
+                this.$router.replace(this.path)
+                //console.log(this.$route)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .tab-bar-item {
+        flex: 1;
+        text-align: center;
+        height: 49px;
+        font-size: 14px;
+    }
+    .tab-bar-item img{
+        width: 20px;
+        height: 20px;
+        margin-top: 3px;
+        vertical-align: middle;
+        margin-bottom: 2px;
+    }
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <TabBar>
+            <TabBarItem path="/home">
+                <img slot="item-icon" src="~/assets/img/tabbar/home.svg">
+                <img slot="item-iconf" src="~/assets/img/tabbar/homef.svg">
+                <div slot="item-text">首页</div>
+            </TabBarItem>
+            <TabBarItem path="/category">
+                <img slot="item-icon" src="~/assets/img/tabbar/sort.svg">
+                <img slot="item-iconf" src="~/assets/img/tabbar/sortf.svg">
+                <div slot="item-text">分类</div>
+            </TabBarItem>
+            <TabBarItem path="/cart">
+                <img slot="item-icon" src="~/assets/img/tabbar/shopcar.svg">
+                <img slot="item-iconf" src="~/assets/img/tabbar/shopcarf.svg">
+                <div slot="item-text">购物车</div>
+            </TabBarItem>
+            <TabBarItem path="/profile">
+                <img slot="item-icon" src="~/assets/img/tabbar/me.svg">
+                <img slot="item-iconf" src="~/assets/img/tabbar/mef.svg">
+                <div slot="item-text">我的</div>
+            </TabBarItem>
+        </TabBar>
+    </div>
+</template>
+
+<script>
+    import TabBar from "components/common/tabbar/TabBar";
+    import TabBarItem from "components/common/tabbar/TabBarItem";
+    export default {
+        name: "MainTabBar",
+        components: {
+            TabBar,
+            TabBarItem
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
